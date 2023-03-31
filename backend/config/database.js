5066583e65fd71ae0fb1365bc777c2f8a9a3f427
aakj2010 <aakj2010@gmail.com>
2023-03-31 14:38:52 +0530

    database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.set("strictQuery", true);
 
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(conn => {
-        console.log(`MonoDB is Connected to host: ${conn.connection.host}`);
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log(`MongoDB is connected to the host: ${con.connection.host} `)
     })
-    // .catch(()=>{
-    //     console.log(error);
-    // })
 }
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
