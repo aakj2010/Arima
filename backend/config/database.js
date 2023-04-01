39da5f302d4b24b3c4a782bc20ddf87ba4133d87
aakj2010 <aakj2010@gmail.com>
2023-04-01 10:03:55 +0530

    add env files, datab

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(con => {
-        console.log(`MongoDB is connected to the host: ${con.connection.host} `)
-    })
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('Error connecting to MongoDB', error));
 }
 
 module.exports = connectDatabase;
